19e50796da7194cb7f0d7c2e35b851b1cbd824b3
OmelianenkoAlexey <alex.lux7777777@gmail.com>
2022-10-29 13:40:52 +0300

    first

---
+++ script.js
@@ -0,0 +1,19 @@
+//console.log это зарезервировання команда JavaSript в броузере
+
+// console.log("Hello,World!", "My name Alex");
+// console.log("Hello,World!", "My name Sasha");
+// console.log("Hello,World!", "My name Dima");
+
+//alert (ошибка) это метод который умеет показывать всплывающие окна
+//Браузерное сообщение, уведомление
+
+// alert("Hello, World!")
+// alert("I'm 39 years old!")
+
+//simple variables (простые переменные)
+milk = 1;
+eggs = 10;
+bread = 1;
+
+console.log(eggs);
+console.log(milk, eggs,)
